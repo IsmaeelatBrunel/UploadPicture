278e6d3d55dd6ffc2da3db59b1007b4ed5ca428b
IsmaeelatBrunel <1708632@brunel.ac.uk>
2019-02-10 16:20:21 +0000

    Initial commit.

---
+++ app/src/main/java/com/example/uploadpicture/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.uploadpicture;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView imageToUpload, imagetoDownload;
+    Button bUploadImage, bDownloadImage;
+    EditText uploadImageName, downloadImageName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+        imagetoDownload = (ImageView) findViewById(R.id.imageToDownload);
+
+        bUploadImage = (Button) findViewById(R.id.bUploadImage);
+        bDownloadImage = (Button) findViewById(R.id.bDownloadImage);
+
+        uploadImageName = (EditText) findViewById(R.id.etUploadName);
+        downloadImageName = (EditText) findViewById(R.id.etDownloadName);
+
+        imageToUpload.setOnClickListener(this);
+        bUploadImage.setOnClickListener(this);
+        bDownloadImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imageToUpload:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                break;
+
+            case R.id.bUploadImage:
+                Bitmap image = ((BitmapDrawable) imageToUpload.getDrawable()).getBitmap();
+
+                break;
+
+            case R.id.bDownloadImage:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }
+    }
+
+}
